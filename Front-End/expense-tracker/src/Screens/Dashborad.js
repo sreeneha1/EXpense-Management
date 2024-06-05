a835f21320677455b519b3f8cd9b5403d6408f62
PravarshaErodula6 <suryasanjeevapravarsha.erodula@slu.edu>
2024-06-04 19:29:18 -0500

    Login Frontend

---
+++ Front-End/expense-tracker/src/Screens/Dashborad.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashborad() {
+  return (
+    <>
+    <div className="d-flex">
+    <div className="navigation">
+          </div>
+          <div>Dashboard</div>
+          </div>
+    </>
+  )
+}
+
+export default Dashborad
