a7ee8817f6b2f813ff304f404b2b5623696d087d
GayuNJ <gayatrinitin.jawharkar@slu.edu>
2024-06-11 22:08:50 -0500

    changes as per styling

--- Front-End/expense-tracker/src/Components/Navigation.js
+++ Front-End/expense-tracker/src/Components/Navigation.js
@@ -2,57 +2,18 @@ import React from "react";
 import "../styles/navigation.css";
 import { Link } from "react-router-dom";
 
-
 function Navigation() {
   return (
-    <div className="nav-bg text-white" style={{ minHeight: "100vh"}}>
-      <div className="title p-2" >
-        {" "}
-        Expense Tracker
-
+    <div className="nav-bg">
+      <div className="title p-2">XpenseTrack</div>
+      <div className = "nav-links">
+          <Link to="/" className="nav-item">Dashboard</Link>
+          <Link to="/inventory" className="nav-item">Inventory</Link>
+          <Link to="/expense" className="nav-item">Expense</Link>
       </div>
-      <div style={{ paddingLeft: "12%", marginTop: "10%" }}>
-        <div className="nav-links">
-          <Link to="/" className="nav-item">
-            Dashboard{" "}
-          </Link>
-        </div>
-        <div className="nav-links">
-          <Link to="/inventory" className="nav-item">
-          Inventory{""}
-          </Link>
-        </div>
-        <div className="nav-links">
-          <Link to="/expense" className="nav-item">
-            Expense
-          </Link>
-        </div>
-        <div className="nav-links">
-          <Link to="/income" className="nav-item">
-            Income
-          </Link>
-        </div>
-      </div>
-      <div
-        style={{
-          marginLeft: "17px",
-          marginRight: "17px",
-          position: "absolute",
-          bottom: "0",
-        }}
-      >
-        <div className="hz-rule"></div>
-        <div
-          className="d-flex mt-2 mb-2"
-          style={{ paddingLeft: "30px", paddingRight: "30px" }}
-        >
-          <div className="profile-icon">IN</div>
-          <div
-            style={{ textAlign: "center", paddingTop: "5%", width: "110px" }}
-          >
-            UserName
-          </div>
-        </div>
+      <div className="profile-container">
+        <div className="profile-icon">IN</div>
+        <div className="profile-name">UserName</div>
       </div>
     </div>
   );
