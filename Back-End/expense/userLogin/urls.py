477ed3ca9f9b0efcd835ab1924b3537a24d1643a
Tanmayee17 <vbachinappa@slu.edu>
2024-06-13 18:17:06 -0500

    Successfully added the logout feature in the backend

--- Back-End/expense/userLogin/urls.py
+++ Back-End/expense/userLogin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import LogoutView
 
 urlpatterns = [
     path("register",views.register_user,name="register_user"),
     path('home', views.Home.as_view()),
-] 
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
--- Back-End/expense/userLogin/views.py
+++ Back-End/expense/userLogin/views.py
@@ -1,6 +1,6 @@
 from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth.models import User
@@ -8,7 +8,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
 @csrf_exempt
 @require_POST
@@ -17,10 +18,9 @@ def register_user(request):
     username = request_body['username']
     password = request_body['password']
     email = request_body["email"]
-    owner = User.objects.create_user(username = username, password = password,email = email)
+    owner = User.objects.create_user(username=username, password=password, email=email)
     owner.save()
-    return JsonResponse({"sucess":True})
-
+    return JsonResponse({"success": True})
 
 class Home(APIView):
     authentication_classes = [JWTAuthentication]
@@ -29,3 +29,19 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+class LogoutView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Logout successful."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+def home_view(request):
+    return HttpResponse("Welcome to the Home Page")
