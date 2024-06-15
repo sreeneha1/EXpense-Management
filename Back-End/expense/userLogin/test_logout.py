620db5c9ca009c97396742441e28ae9f447f9782
Tanmayee17 <vbachinappa@slu.edu>
2024-06-14 20:10:29 -0500

    successfully ran the backend test cases without errors for logout functionality.

--- Back-End/expense/userLogin/test_logout.py
+++ Back-End/expense/userLogin/test_logout.py
@@ -36,5 +36,5 @@ class TestLogout(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_logout_with_missing_refresh_token(self):
-        response = self.client.post(reverse('logout'), {})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            response = self.client.post(reverse('logout'), {})
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
