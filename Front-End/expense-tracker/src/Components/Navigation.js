229c11c9fc86e303d06f2a019c797acb060720ca
sreeneha1 <yashaswinisreeneha.garapati@slu.edu>
2024-06-15 01:28:57 -0500

    Changes in Navigation.js and AUth.js

--- Front-End/expense-tracker/src/Components/Navigation.js
+++ Front-End/expense-tracker/src/Components/Navigation.js
@@ -3,17 +3,25 @@ import "../styles/navigation.css";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const username = localStorage.getItem("username");
+
   return (
     <div className="nav-bg">
       <div className="title p-2">XpenseTrack</div>
-      <div className = "nav-links">
-          <Link to="/" className="nav-item">Dashboard</Link>
-          <Link to="/inventory" className="nav-item">Inventory</Link>
-          <Link to="/expense" className="nav-item">Expense</Link>
+      <div className="nav-links">
+        <Link to="/" className="nav-item">Dashboard</Link>
+        <Link to="/inventory" className="nav-item">Inventory</Link>
+        <Link to="/expense" className="nav-item">Expense</Link>
       </div>
       <div className="profile-container">
-        <div className="profile-icon">IN</div>
-        <div className="profile-name">UserName</div>
+        {username ? (
+          <>
+            <div className="profile-icon">{username[0].toUpperCase()}</div>
+            <div className="profile-name">{username}</div>
+          </>
+        ) : (
+          <div className="profile-placeholder">Not logged in</div>
+        )}
       </div>
     </div>
   );
